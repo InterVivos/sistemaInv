d6848c455abacf4ef0c760a5e53fa34480db2896
InterVivos <112789674+InterVivos@users.noreply.github.com>
2022-11-17 15:15:08 -0500

    first commit

---
+++ src/main/java/sistemaInv/Principal.java
@@ -0,0 +1,15 @@
+package sistemaInv;
+
+import javax.swing.*;
+
+public class Principal
+{
+	public static void main(String[] args)
+	{
+		JFrame ventana = new JFrame("Hola mundo");
+		ventana.setSize(200, 200);
+		ventana.setVisible(true);
+		ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
